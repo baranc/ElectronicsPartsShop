6b4aac4d83349a936fc4ac812b0c8bd3c79083b9
Adam Baranczyk <baranczykadam@gmail.com>
2025-01-25 09:48:31 +0100

    Migration od database

--- ElectronicsPartsShop.Server/Data/ShopDbContext.cs
+++ ElectronicsPartsShop.Server/Data/ShopDbContext.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ElectronicsPartsShop.Server.Models;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Core.Entities;
 
 namespace ElectronicsPartsShop.Server.Data
 {
-    public class ShopDbContext : DbContext
+    public class ShopDbContext : IdentityDbContext<AppUser>
     {
         public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<PurchaseItem> PurchaseItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfiguracja relacji między Purchase a IdentityUser
+            modelBuilder.Entity<Purchase>()
+                .HasOne(p => p.User)
+                .WithMany()
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Konfiguracja relacji między Purchase a PurchaseItem
+            modelBuilder.Entity<Purchase>()
+                .HasMany(p => p.Items)
+                .WithOne(pi => pi.Purchase)
+                .HasForeignKey(pi => pi.PurchaseId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Konfiguracja relacji między PurchaseItem a Product
+            modelBuilder.Entity<PurchaseItem>()
+                .HasOne(pi => pi.Product)
+                .WithMany()
+                .HasForeignKey(pi => pi.ProductId);
+        }
     }
 }
